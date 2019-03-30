f7f2953030750463c134686cdeb67974eeee2ed0
Simba-cheng <simba_cheng@163.com>
2019-03-30 22:42:03 +0800

    初始化

---
+++ src/main/java/org/algorithm/App.java
@@ -0,0 +1,13 @@
+package org.algorithm;
+
+/**
+ * @author CYX
+ * @create 2019-03-30-22:38
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("algorithm");
+    }
+
+}
